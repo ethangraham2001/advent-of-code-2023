bba899d5bffe603b4c327f0cda725f19d11132d8
Ethan <ethan.graham@epfl.ch>
2023-12-03 00:06:12 +0100

    updated gitignore, and completed day01

--- day-1/trebuchet.cpp
+++ day-1/trebuchet.cpp
@@ -1,10 +1,16 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <unordered_map>
 
 #define FILENAME "trebuchet_input.txt"
 
-int solution(std::string filename)
+/**
+ * @brief solves part 2
+ *
+ * @param filename name of input file
+ */
+int solution_part1(std::string filename)
 {
     int res = 0;
     std::fstream file(filename);
@@ -30,9 +36,79 @@ int solution(std::string filename)
     return res;
 }
 
+#define IS_DIG(c) ((c) >= '0' && (c) <= '9')
+
+/**
+ * @brief solves part 2
+ *
+ * @param filename name of input file
+ */
+int solution_part2(std::string filename)
+{
+    /* string-to-digit conversion */
+    std::unordered_map<std::string, int> str_to_int = {
+        {"one",   1},
+        {"two",   2},
+        {"three", 3},
+        {"four",  4},
+        {"five",  5},
+        {"six",   6},
+        {"seven", 7},
+        {"eight", 8},
+        {"nine",  9},
+    };
+
+    int res = 0;
+    std::fstream file(filename);
+    std::string line;
+
+    while (getline(file, line))
+    {
+        int first = -1;
+        int last = -1;
+        int i = 0; 
+        while (i < line.size())
+        {
+            int value = -1;
+
+            // search for value starting at current position
+            if (IS_DIG(line.at(i))) {
+                value = line.at(i) - '0';
+            } else {
+                int j = i + 1; 
+
+                while (j < line.size() && !IS_DIG(line.at(j)))
+                {
+                    std::string substr = line.substr(i, j-i+1);
+
+                    if (str_to_int[substr]) {
+                        value = str_to_int[substr];
+                        i = j - 1;
+                        break;
+                    }
+
+                    j++;
+                }
+            }
+            i++;
+            if (value > 0) {
+                if (first < 0) {
+                    first = value;
+                }
+
+                last = value;
+            }
+        }
+
+        res += first*10 + last;
+    }
+    return res;
+}
+
 int main()
 {
-    std::cout << solution(FILENAME) << "\n";
+    std::cout << "Part 1: " << solution_part1(FILENAME) << "\n";
+    std::cout << "Part 2: " << solution_part2(FILENAME) << "\n";
 
     return 0;
 }
