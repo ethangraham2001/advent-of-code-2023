7ae536c765c343ce113b7df76bd0bac70fc923a9
Ethan <ethan.graham@epfl.ch>
2023-12-02 21:03:47 +0100

    removing accidental completion of 2015 questions lol

--- day-2/no_math.cpp
+++ day-2/no_math.cpp
@@ -1,35 +1,25 @@
 #include <iostream>
-#include <vector>
-#include <fstream>
-#include <algorithm>
+#include <fsteam>
+#include <string>
 
-template <typename T1, typename T2, typename T3>
-class Triplet {
-public:
-    T1 first;
-    T2 second;
-    T3 third;
+#define FILENAME "input.txt"
 
-    Triplet(const T1& f, const T2& s, const T3& t) : first(f), second(s), third(t) {}
-};
-
-std::vector<Triplet<int, int, int>> parse_input(std::string filename)
+/**
+ * @brief solves the problem
+ */
+int solution_day2(std::string filename)
 {
-    std::vector<Triplet<int, int, int>> out;
-    std::fstream file(filename);
+    std::ifstream file(filename);
 
-    if (!file.is_open())
-    {
-        std::cerr << "unable to open file\n" << std::endl;
-    }
+    if (!file.isopen())
+        return -1;
 
     std::string line;
-
-    while (std::getline(file, line))
+    while(getline(file, line))
     {
-        int l, w, h;
         int first_x = -1;
         int second_x = -1;
+
         for (int i = 0; i < line.size(); i++)
         {
             if (line.at(i) == 'x' && first_x < 0)
@@ -37,46 +27,11 @@ std::vector<Triplet<int, int, int>> parse_input(std::string filename)
             else if (line.at(i) == 'x')
                 second_x = i;
         }
-
-        l = std::stoi(line.substr(0, first_x));
-        w = std::stoi(line.substr(first_x+1, second_x - first_x - 1));
-        h = std::stoi(line.substr(second_x+1, line.size() - second_x - 1));
-        out.push_back(Triplet(l, w, h));
     }
-
-    return out;
 }
 
-unsigned long int solution_part1(std::vector<Triplet<int, int, int>> problem_input)
-{
-    unsigned long int output = 0;
-
-    for (auto triplet : problem_input)
-    {
-        int l = triplet.first;
-        int w = triplet.second;
-        int h = triplet.third;
-        
-        int side_0 = l*w;
-        int side_1 = l*h;
-        int side_2 = w*h;
-
-        int min_dim= std::min({l, w, h});
-        output += side_0 + side_1 + side_2;
-        // output *=2;
-        // output += min_dim;
-    }
-
-    return 2*output;
-}
 
 int main()
 {
-    std::vector<Triplet<int, int, int>> problem_input = parse_input("input.txt");
 
-    int output = solution_part1(problem_input);
-    std::cout << "Part 1: " << output << "\n";
-
-    return 0;
 }
-
